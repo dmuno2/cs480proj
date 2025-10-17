b301734540cb63872b463712975153977ef4d429
David Munoz <118083632+dmuno2@users.noreply.github.com>
2025-10-17 09:53:50 -0500

    name change

--- db_conn.py
+++ db_conn.py
@@ -4,7 +4,7 @@ def get_db_connection():
     conn = psycopg2.connect(
         host="ep-snowy-mouse-a484zcoz-pooler.us-east-1.aws.neon.tech",  # from your Neon connection screen
         database="neondb",  # your database name
-        user="Shayan",  # from your env.txt, PGUSER=Shayan
+        user="David",  # from your env.txt, PGUSER=Shayan,David
         password="npg_fU8HrEaCt2TD",  # from your env.txt, PGPASSWORD=...
         port=5432,
         sslmode='require'  # Neon requires SSL
